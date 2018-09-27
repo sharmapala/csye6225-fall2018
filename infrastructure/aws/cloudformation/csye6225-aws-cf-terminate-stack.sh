1909cc20d636d80ed7e67d6b3ffa7177bcef9a77
Yaggesh Likhar <yaggeshlikhar@localhost.localdomain>
2018-09-27 16:42:51 -0400

    Cloud formation terminate script

--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
-[ $# -eq 0 ] && { echo -e "\nUsage `basename $0` <Stack Name>\n"; exit 1; }
+#[ $# -eq 0 ] && { echo -e "\nUsage `basename $0` <Stack Name>\n"; exit 1; }
+
+aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE||CREATE_IN_PROGRESS||REVIEW_IN_PROGRESS||DELETE_IN_PROGRESS||DELETE_FAILED||UPDATE_IN_PROGRESS||DELETE_COMPLETE
+
+echo "Enter the stack you want to delete"
+read Stack_Name
 
 #Inputs
-stackName=${1?param missing - Stack Name}
+#=${1?param missing - Stack Name}
 # Stack_Name=${1?Error: Enter correct Stackname}
-
 
 # Delete the Stack
 
-aws cloudformation delete-stack --stack-name ${stackName}
+aws cloudformation delete-stack --stack-name $Stack_Name
