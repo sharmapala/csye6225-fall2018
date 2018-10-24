e6bf60325d6fc3366ac62dcefb8dd60ac4e2982c
Palak Sharma <sharma.pala@husky.neu.edu>
2018-10-24 02:24:10 -0400

    giving relative paths

--- webapp/server/models/database.js
+++ webapp/server/models/database.js
@@ -25,9 +25,9 @@ sequelize = new Sequelize('registeredMembers', 'root', '', {
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.users = require("../models/user/user.schema.server")(sequelize, Sequelize);
-db.transactions = require("../models/transaction/transaction.schema.server")(sequelize, Sequelize);
-db.attachments = require("../models/attachment/attachment.schema.server")(sequelize, Sequelize);
+db.users = require(__dirname+"/user/user.schema.server")(sequelize, Sequelize);
+db.transactions = require(__dirname+"/transaction/transaction.schema.server")(sequelize, Sequelize);
+db.attachments = require(__dirname+"/attachment/attachment.schema.server")(sequelize, Sequelize);
  // for local
 // if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
 //     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
@@ -35,11 +35,11 @@ db.attachments = require("../models/attachment/attachment.schema.server")(sequel
 //     connectionString = 'mongodb://' + username + ':' + password;
 //     connectionString += '@ds149382.mlab.com:49382/heroku_v33hz0kp'; // user yours
 // }
-// db.users.sync();
-// db.attachments.sync();
-// db.transactions.sync();
-// db.users.hasMany(db.transactions);
-// db.transactions.hasMany(db.attachments);
+db.users.sync();
+db.attachments.sync();
+db.transactions.sync();
+db.users.hasMany(db.transactions);
+db.transactions.hasMany(db.attachments);
 // .authenticate()
 // .then(() => {
 //   console.log('Connection has been established successfully.');
