96a803eab2111372b6741c4108fce5d248ab9601
Palak Sharma <sharma.pala@husky.neu.edu>
2018-10-25 13:53:00 -0400

    Adding npm module dotenv

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -5,6 +5,7 @@ sudo cp .env webapp
 cd webapp
 sudo chmod 666 .env
 npm install
+npm install dotenv 
 
 # setup NODE_ENV
 #export RDS_HOSTNAME=csye6225-fall2018.coh3ahyiuez7.us-east-1.rds.amazonaws.com
